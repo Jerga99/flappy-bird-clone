51e3a10e3955a1a90613cd049ceef77bdc7bbbe4
Filip Jerga <jerga99@gmail.com>
2020-10-12 14:59:22 +0200

    7 2 Velocity

--- src/index.js
+++ src/index.js
@@ -14,6 +14,7 @@ const config = {
   scene: {
     preload,
     create,
+    update
   }
 }
 
@@ -36,8 +37,16 @@ function create() {
   // this.add.image(0, 0, 'sky').setOrigin(0, 0);
   this.add.image(0, 0, 'sky').setOrigin(0);
   // middle of the height , 1/10 width
-  bird = this.add.sprite(config.width * 0.1, config.height / 2, 'bird').setOrigin(0)
-  debugger
+  bird = this.physics.add.sprite(config.width * 0.1, config.height / 2, 'bird').setOrigin(0)
+
+  bird.body.gravity.y = 200;
+}
+
+// 60fps
+// 60 times per second
+// 60 * 16ms = 1000ms
+function update(time, delta) {
+  console.log(bird.body.velocity.y);
 }
 
 
