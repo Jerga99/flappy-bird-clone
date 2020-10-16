edb564fdfdb0fd03bf91d0cbe929cda7ee0f7612
Filip Jerga <jerga99@gmail.com>
2020-10-16 10:12:15 +0200

    Countdown

--- src/scenes/PlayScene.js
+++ src/scenes/PlayScene.js
@@ -37,16 +37,28 @@ class PlayScene extends BaseScene {
   }
 
   listenToEvents() {
-    this.events.on('resume', () => {
+    if (this.pauseEvent) { return; }
+
+    this.pauseEvent = this.events.on('resume', () => {
       this.initialTime = 3;
       this.countDownText = this.add.text(...this.screenCenter, 'Fly in: ' + this.initialTime, this.fontOptions).setOrigin(0.5);
       this.timedEvent = this.time.addEvent({
         delay: 1000,
-        callback: () => console.log(this.initialTime--),
+        callback: this.countDown,
         callbackScope: this,
         loop: true
       })
     })
+  }
+
+  countDown() {
+    this.initialTime--;
+    this.countDownText.setText('Fly in: ' + this.initialTime);
+    if (this.initialTime <= 0) {
+      this.countDownText.setText('');
+      this.physics.resume();
+      this.timedEvent.remove();
+    }
   }
 
   createBG() {
